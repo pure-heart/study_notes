836bb04e65c21470fcbefb521fc4669e66556004
Jhly <13545175547@163.com>
2019-10-05 23:25:24 +0800

    类和对象

---
+++ javacore/Chapter04/src/main/java/JDate.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * @Auther:JHLY
+ * @Date:2019/10/5
+ * @Description:PACKAGE_NAME
+ * @Version:1.0
+ */
+public class JDate {
+    public static void main(String[] args) {
+        System.out.println(new Date().toString());
+    }
+}
