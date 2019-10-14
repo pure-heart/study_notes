79be8a4f08bc6f16dcaf9991cb495433231f66f0
Jhly <13545175547@163.com>
2019-10-14 13:34:39 +0800

    并发

---
+++ javacore/chapter14/src/main/java/threadPool/ThreadPoolTest.java
@@ -0,0 +1,104 @@
+package threadPool;
+
+import com.sun.xml.internal.ws.util.Pool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+/**
+ * 线程池
+ *
+ * @Auther:JHLY
+ * @Date:2019/10/14
+ * @Description:threadPool
+ * @Version:1.0
+ */
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter base directory (e.g. /user/local/jdk5.0/src):");
+            String directory = in.nextLine();
+            System.out.println("Enter keyword (e.g. volatile):");
+            String keyword = in.nextLine();
+
+            ExecutorService pool = Executors.newCachedThreadPool();
+
+            MatchCounter counter = new MatchCounter(new File(directory), keyword, pool);
+            Future<Integer> result = pool.submit(counter);
+
+            try {
+                System.out.println(result.get()+"matching files.");
+            }catch (ExecutionException e){
+                e.printStackTrace();
+            }catch (InterruptedException e)
+            {
+            }
+            pool.shutdown();
+            int largestPoolSize = ((ThreadPoolExecutor)pool).getLargestPoolSize();
+            System.out.println("largest pool size = "+largestPoolSize);
+        }
+
+    }
+}
+
+class MatchCounter implements Callable<Integer> {
+    private File directory;
+    private String keyword;
+    private ExecutorService pool;
+    private int count;
+
+    public MatchCounter(File directory, String keyword, ExecutorService pool) {
+        this.directory = directory;
+        this.keyword = keyword;
+        this.pool = pool;
+    }
+
+    @Override
+    public Integer call() {
+        count = 0;
+        try {
+            File[] files = directory.listFiles();
+            List<Future<Integer>> results = new ArrayList<>();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    MatchCounter counter = new MatchCounter(file, keyword, pool);
+                    Future<Integer> result = pool.submit(counter);
+                    results.add(result);
+                } else {
+                    if (search(file)) count++;
+                }
+            }
+            for (Future<Integer> result : results) {
+                try {
+                    count += result.get();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (InterruptedException e) {
+
+        }
+        return count;
+    }
+
+    public boolean search(File file) {
+        try (Scanner in = new Scanner(file, "UTF-8")) {
+            boolean found = false;
+            while (!found && in.hasNext()) {
+                String line = in.nextLine();
+                if (line.contains(keyword)) found = true;
+            }
+            return found;
+        } catch (IOException e) {
+            return false;
+        }
+
+    }
+}
+
